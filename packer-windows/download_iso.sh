ded766d2837a1a3ed704b8f8302130d0b9891fce
jollaman999 <admin@jollaman999.com>
2022-04-05 18:26:43 +0900

    Try to make Windows Vagrant box with Packer

---
+++ packer-windows/download_iso.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -c "https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso" -O virtio-win.iso
+wget -c "https://heize.jollaman999.com:19400/temp/Win10_21H2_Korean_x64.iso" -O Win10_21H2_Korean_x64.iso
