6839bc65d0c4c8630528a88d2fcaee8f03ddf0d0
jollaman999 <admin@jollaman999.com>
2022-04-06 14:22:02 +0900

    packer-windows: Fix of changing powerplan

--- packer-windows/scripts/set-powerplan.ps1
+++ packer-windows/scripts/set-powerplan.ps1
@@ -1,11 +1,11 @@
 ﻿Try {
-  Write-Output "Set power plan to high performance"
+  Write-Output "Set power plan to 고성능"
 
-  $HighPerf = powercfg -l | ForEach-Object { if ($_.contains("High performance")) { $_.split()[3] } }
+  $HighPerf = powercfg -l | ForEach-Object { if ($_.contains("고성능")) { $_.split()[3] } }
 
   # $HighPerf cannot be $null, we try activate this power profile with powercfg
   if ($null -eq $HighPerf) {
-    throw "Error: HighPerf is null"
+    throw "Error: 고성능 is null"
   }
 
   $CurrPlan = $(powercfg -getactivescheme).split()[3]
@@ -14,6 +14,6 @@
 
 }
 Catch {
-  Write-Warning -Message "Unable to set power plan to high performance"
+  Write-Warning -Message "Unable to set power plan to 고성능"
   Write-Warning $Error[0]
 }
